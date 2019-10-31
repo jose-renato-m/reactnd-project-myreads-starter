797531c06682c1243f9d969306bb957983403b4e
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-31 12:44:00 -0300

    chore: Add a comment about package installation
    In order to import BrowserRouter from the library, before it has been
    installed the react-router-dom via node package management (npm). Thus
    it is important to explain from where this library is being imported.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// react router dom has been installed through node package manager
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import './index.css';
