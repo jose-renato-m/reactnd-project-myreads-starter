26f8285a130b04008472c8c8030ecca4cdb8a2e0
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-30 21:44:23 -0300

    feat: Paste homepage jsx block

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,7 +2,15 @@ import React, {Component} from 'react';
 
     class Home extends Component {
        render() {
-           return (<h1></h1>)
+           return (<div className="list-books">
+           <div className="list-books-title">
+             <h1>MyReads</h1>
+           </div>
+           <div className="list-books-content">
+             <Shelf />              
+           </div>
+           <FAB />               
+         </div>)
        } 
     }
 
