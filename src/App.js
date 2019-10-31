947c54fb52b5df3ae7daaae1fc3e574e663a0ea1
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-31 15:15:21 -0300

    style: Replace dot to comma

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch.Route} from 'react-router-dom';
+import {Switch,Route} from 'react-router-dom';
 // import * as BooksAPI from './BooksAPI'
 import Home from './views/Home';
 import Search from './views/Search';
