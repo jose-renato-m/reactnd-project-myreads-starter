580f7bec2dcaa5b69c2dbae3426a554da1d0b33c
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-05 17:29:03 -0300

    feat: Add constructor

--- src/provider/index.js
+++ src/provider/index.js
@@ -2,6 +2,17 @@ import React, {Component} from 'react';
 export const MyContext = React.createContext();
 
     class index extends Component {
+        constructor() {
+            super();
+            this.state = {
+                books:[],
+                currentlyReading:[],
+                wantToRead:[],
+                read:[]
+            }
+        }
+
+
         render() {
             return (<h1></h1>)
         }
