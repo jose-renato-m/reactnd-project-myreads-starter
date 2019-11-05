d67ac5f7b4550b290e92f5c08ae530cbadb6a2b1
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-05 15:08:44 -0300

    feat: Improve react router
    Due that the previous code was displaying a white screen with nothing
    displayed on it and according to udacity's mentor orientation, it was
    looked for React Router documents at
    reacttraing.com/react-router/web/guides/quick-start, trying to improve
    the code concerning to React Router. In a first approach, it was
    successful to display some of what the project asks for, but still is
    not ideal.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Home from './views/Home';
 import Search from './views/Search';
 import './App.css';
   class BooksApp extends React.Component {
     render() {
       return (
-        <div className="app">
-        <Switch>
-          <Route exact path="/" Component={Home} />
-          <Route path="/search" Component={Search} />
-        </Switch>        
-        </div>
-      )
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/Search">Search</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+            <Switch>
+              <Route path="/search">
+                <Search />
+              </Route>
+            </Switch>
+          </div>
+        </Router>        
+      );
     }
   }
 
