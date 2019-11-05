781bbb7bb66b5a3da304c9ab3e1d65c60f3c32e2
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-05 16:12:28 -0300

    style: Improve indention

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -3,14 +3,16 @@ import Book from './Book';
 
     class Shelf extends Component {
         render() {
-            return (                <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                <Book />
-              </ol>
+          return (                
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <Book />
+                </ol>
+              </div>
             </div>
-          </div>)
+          )
         }
     }
      
