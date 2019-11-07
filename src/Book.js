1624d9559e0a0dbdccace2db0c345797de3bddbe
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-07 20:28:21 -0300

    refactor: Relocate jsx block

--- src/Book.js
+++ src/Book.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 
     class Book extends Component {
         render() {
-            return (<li>
-                <div className="book">
+            return (
+              <div className="book">
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                     <div className="book-shelf-changer">
@@ -19,6 +19,9 @@ import React, {Component} from 'react';
                   <div className="book-title">To Kill a Mockingbird</div>
                   <div className="book-authors">Harper Lee</div>
                 </div>
+            
+            <li>
+                
               </li>)
         }
     }
