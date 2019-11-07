807b18fc84b90d94912e39564ed88746f0529997
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-07 19:54:03 -0300

    refactor: Delete 'shelf' jsx block

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Book from './Book';
-import Shelf from './components/Shelf';
 import FAB from './components/FAB';
 
   class Home extends Component {     
@@ -10,11 +9,7 @@ import FAB from './components/FAB';
           <div className="list-books-title">
             <h1>MyReads</h1>
           </div>
-          <div className="list-books-content">
-            <Shelf title="Currently Reading" />
-            <Shelf title="Want to Read" />
-            <Shelf title="Read" />
-          </div>
+          
           <FAB />               
         </div>
       );
