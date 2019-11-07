e4656c65530a39cdae9bd2d5b0f9075c06cb411e
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-07 17:56:10 -0300

    refactor: Import 'Search' from another folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Home';
-import Search from './views/Search';
+import Search from './Search';
 import './App.css';
   class BooksApp extends React.Component {
     render() {
