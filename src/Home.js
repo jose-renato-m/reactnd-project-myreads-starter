a74fd8882a33cfa583641109a63e4433f92b4c60
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-07 19:56:54 -0300

    refactor: Delete 'FAB' component

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Book from './Book';
-import FAB from './components/FAB';
 
   class Home extends Component {     
     render() {
@@ -8,9 +7,7 @@ import FAB from './components/FAB';
         <div className="list-books">
           <div className="list-books-title">
             <h1>MyReads</h1>
-          </div>
-          
-          <FAB />               
+          </div>                        
         </div>
       );
     } 
