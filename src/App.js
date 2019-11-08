46fa7425b7057dc095c8fd92c31c5d012b9b4cc6
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-08 14:49:59 -0300

    refactor: Delete two state code lines

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Home from './Home';
 import Search from './Search';
 import './App.css';
 
-  class BooksApp extends React.Component {
-    state = {
-      showSearchPage: false
-    }
-
+  class BooksApp extends React.Component {    
     render() {
       return (
         <div className="app">            
