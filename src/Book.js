920c101ed26cb93009935b617d571986eb7bcb4e
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-08 19:52:56 -0300

    refactor: Change inside function it was wrong

--- src/Book.js
+++ src/Book.js
@@ -8,7 +8,9 @@ import React, {Component} from 'react';
                   <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
                   <div className="book-shelf-changer">
                     <select
-                      onChange={this.props.changeShelf()}
+                      onChange={(event) => this.props.changeShelf(
+                        this.props.book, event.target.value
+                      )}
                     >
                       <option value="move" disabled>Move to...</option>
                       <option value="currentlyReading">Currently Reading</option>
