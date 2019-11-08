ab647a625a333ef025ec2d858a9401db69d9a7d7
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-08 15:45:50 -0300

    feat: Import 'BooksAPI' from library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './Home';
 import Search from './Search';
+import * as BooksAPI from './BooksAPI';
 import './App.css';
 
   class BooksApp extends React.Component {
