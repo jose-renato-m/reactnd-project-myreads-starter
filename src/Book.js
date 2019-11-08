91856770d11a5d8c93553002dd2270ec4cf8fed1
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-08 19:27:32 -0300

    feat: Receive props from parent

--- src/Book.js
+++ src/Book.js
@@ -8,7 +8,7 @@ import React, {Component} from 'react';
                   <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
                   <div className="book-shelf-changer">
                     <select
-                      onChange={}
+                      onChange={this.props.changeShelf()}
                     >
                       <option value="move" disabled>Move to...</option>
                       <option value="currentlyReading">Currently Reading</option>
