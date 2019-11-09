c2820c68961105c672d8bfc95e699da4a5674aa9
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-09 17:40:19 -0300

    feat: Add value into input component

--- src/Search.js
+++ src/Search.js
@@ -16,8 +16,14 @@ import React, {Component} from 'react';
                 <div className="search-books">
                 <div className="search-books-bar">
                   <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-                <div className="search-books-input-wrapper">                
-                  <input type="text" placeholder="Search by title or author"/>
+                <div className="search-books-input-wrapper">
+
+                  <input 
+                    type="text"
+                    placeholder="Search by title or author"
+                    value={this.state.query}
+                  />
+
                 </div>
                 </div>
                 <div className="search-books-results">
