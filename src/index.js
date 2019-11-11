07427210387680f85b822d6733096802def5f917
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-11 18:06:31 -0300

    style: Add spaces

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // react router dom has been installed through node package manager
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
