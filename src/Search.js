01127991934bc87778a6c6c06e99e2ac335112c1
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-11 18:22:26 -0300

    feat: Add link to main page

--- src/Search.js
+++ src/Search.js
@@ -34,7 +34,12 @@ import * as BooksAPI from './BooksAPI';
             return (
                 <div className="search-books">
                 <div className="search-books-bar">
-                  <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+
+                  <Link
+                    to="/"
+                    className="close-search"
+                  >Close</Link>
+
                 <div className="search-books-input-wrapper">
 
                   <input 
