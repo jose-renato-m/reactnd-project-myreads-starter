6b240344f213c08c9b74ce14d96ce6ddaea56bc2
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-11 18:34:59 -0300

    feat: Import route from library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import Home from './Home';
 import Search from './Search';
+
 import * as BooksAPI from './BooksAPI';
 import './App.css';
 
