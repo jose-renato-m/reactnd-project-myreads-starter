78a3477f94f1a4b44624fe8335686ffa0fe29559
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-11 18:31:26 -0300

    feat: Add link to search page

--- src/Home.js
+++ src/Home.js
@@ -73,7 +73,9 @@ import Book from './Book';
             </div>
           </div>
           <div className="open-search">
-            <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+            <Link 
+              to="/search"
+            >Add a book</Link>
           </div>
           </div>
       );
