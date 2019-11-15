8a65f7aaacb6c31e02213eecc61db2512384a6b2
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-15 17:04:53 -0300

    feat: Add footer jsx tag

--- src/FooterNav.js
+++ src/FooterNav.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+function FooterNav () {
+    return (
+        <footer className='footer-nav'>
+            
+        </footer>
+    );
+}
+
 export default FooterNav;
