a326fa0a1fa5425dc71fe621cdedeeabd375614b
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-15 17:13:24 -0300

    feat: Add props to stateless component

--- src/FooterNav.js
+++ src/FooterNav.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function FooterNav () {
+function FooterNav (props) {
     return (
         <footer className='footer-nav'>
             <nav>
-                
+            <Link to='/search'>
+          <button onClick={() => this.setState({ showSearchPage: true })}>
+            Add a book
+          </button>
+        </Link>
             </nav>
         </footer>
     );
