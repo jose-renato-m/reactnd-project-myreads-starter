1a1a271f49a02ec844958b1da55a84ed54926b62
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-20 21:10:59 -0300

    style: Delete some spaces between code block

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
 import Home from './Home';
 import Search from './Search';
-
 import * as BooksAPI from './BooksAPI';
 import './App.css';
 
