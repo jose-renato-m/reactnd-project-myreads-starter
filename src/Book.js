d29f66fe3840dce42c5df8ba2c8cdb018c913dc1
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-23 18:19:07 -0300

    feat: Add javascript '.join()' to separate authors

--- src/Book.js
+++ src/Book.js
@@ -26,7 +26,7 @@ import React, {Component} from 'react';
                   </div>
                 </div>
                   <div className='book-title'>{this.props.book.title}</div>
-                  <div className='book-authors'>{this.props.book.authors}</div>
+                  <div className='book-authors'>{this.props.book.authors && this.props.book.authors.join(', ')}</div>                  
                 </div>            
             );
         }
